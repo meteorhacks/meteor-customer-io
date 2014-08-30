f630af1b504403692d67db3588a928def5477b92
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-30 07:19:23 +0530

    added package meta files

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  "summary": "Customer.io Integration for Meteor",
+  "version": "1.0.0",
+  "git": "https://github.com/meteorhacks/meteor-customer.io.git",
+  "name": "meteorhacks:customer.io"
+});
+
+Package.on_use(function(api) {
+  configurePackage(api);
+  api.export(['_cio'], 'client');
+  api.export(['CustomerIo']);
+});
+
+function configurePackage(api) {
+  if(api.versionsFrom) {
+    api.versionsFrom('METEOR@0.9.0');
+  }
+  
+  api.use(['underscore'], ['server', 'client']);
+
+  api.add_files(['server.js'], 'server');
+  api.add_files(['client.js'], 'client')
+}
