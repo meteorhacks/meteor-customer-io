eef742be801f009e3f12d37c833331fa8fd3e5b3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-31 06:15:22 +0530

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Customer.io Integration for Meteor",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "git": "https://github.com/meteorhacks/meteor-customer.io.git",
   "name": "meteorhacks:customer.io"
 });
